afe1f01c11cc32111e19e9774a680390accd7650
Stephane Erard <stephane.erard.se@gmail.com>
2021-02-18 13:00:58 +0100

    k8s.configure & k8s.install: add ufw, fixs

--- provisioning/Debian_64/10.8.0/k8s.install.sh
+++ provisioning/Debian_64/10.8.0/k8s.install.sh
@@ -7,16 +7,20 @@ EOF
 sudo sysctl --system
 
 sudo apt-get -y install apt-transport-https gnupg2 curl 
-
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 
-
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list 
-
 sudo apt-get update
+
 
 sudo apt-get -y install kubeadm kubelet kubectl 
 sudo apt-mark hold kubelet kubeadm kubectl
 
+sudo systemctl daemon-reload
 sudo systemctl enable kubelet 
+sudo systemctl restart kubelet
 
 sudo kubeadm config images pull
+
+sudo apt-get install -y ufw
+sudo ufw status
+sudo ufw enable
